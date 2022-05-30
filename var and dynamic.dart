86b015d0490685a4c676fe8bdf4a4b157c0203cc
Champak's World <champaksworld@gmail.com>
2022-05-30 16:56:01 +0530

    Update var and dynamic.dart

--- var and dynamic.dart
+++ var and dynamic.dart
@@ -1,18 +1,12 @@
 void main() {
-var x1="10";
-  print(x1);
-  print(x1.runtimeType);
-  //x1=10; type cannot be changed
-  dynamic x2="10";
-  print(x2);
-  print(x2.runtimeType);
-  x2=10;//Type can be changed
-  print(x2);
-  print(x2.runtimeType);
-  
+  var varVariable = "10";
+  print("Var variable values $varVariable");
+  print("Var variable type ${varVariable.runtimeType}");
+  //varVariable=10; type cannot be changed
+  dynamic dynamicvariable = "10";
+  print("Dynamic variable value $dynamicvariable");
+  print("Dynamic variable type ${dynamicvariable.runtimeType}");
+  dynamicvariable = 10; //Type can be changed
+  print("Dynamic variable value $dynamicvariable");
+  print("Dynamic variable type ${dynamicvariable.runtimeType}");
 }
-
-     
-
-
-
