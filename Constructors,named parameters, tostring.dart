863e3a6413eaf89c0fc51a6491d37ac162b765f3
Champak's World <champaksworld@gmail.com>
2022-06-04 17:40:46 +0530

    Update Constructors,named parameters, tostring.dart

--- Constructors,named parameters, tostring.dart
+++ Constructors,named parameters, tostring.dart
@@ -1,48 +1,47 @@
 void main() {
- Book b= Book("Basi C","C",100);
+  Book b = Book("The Recursion Sutras", "Recursion", 299);
   print(b);
-  add(1,2);
-  namedadd(n2:7,n1:2);
-  namedadd(n2:7);
-  requireddadd(n2:7);
-  Person p=Person(name:"Sachin",);
+  simpleFunctionwithRequiredParameters(1, 2);
+  functionWithDefaultValueNamedParameters(n2: 7, n1: 2);
+  functionWithDefaultValueNamedParameters(n2: 7);
+
+  functionWithOneRequiredParameter(n2: 7);
+  Person p = Person(
+    name: "Sachin",
+  );
   print(p);
 }
-enum Operation {
-  add,
-  sub
-}
-class Person
-{
+
+enum Operation { add, sub }
+
+class Person {
   String name;
   int age;
-  Person({required this.name,this.age=10});
-   @override
-  String toString()
-  {
-    return "Name = $name, Age = ${this.age}";
+  Person({required this.name, this.age = 10});
+  @override
+  String toString() {
+    return "Person (Name = $name, Age = $age) ";
   }
 }
-void requireddadd({int n1=0,required int n2})
-{
+
+void functionWithOneRequiredParameter({int n1 = 0, required int n2}) {
   print("$n1 + $n2 =${n1 + n2}");
 }
 
-void namedadd({int n1=0, int n2=0})
-{
-  print("$n1 + $n2 =${n1 + n2}");
+void functionWithDefaultValueNamedParameters({int n1 = 0, int n2 = 0}) {
+  print("$n1 + $n2 = ${n1 + n2}");
 }
-void add(int n1,int n2)
-{
-  print("$n1 + $n2 =${n1 + n2}");
+
+void simpleFunctionwithRequiredParameters(int n1, int n2) {
+  print("$n1 + $n2 = ${n1 + n2}");
 }
-class Book{
-  String bookname="",subject;
+
+class Book {
+  String bookname = "", subject;
   int price;
-  Book(this.bookname,this.subject,this.price);
+  Book(this.bookname, this.subject, this.price);
   @override
-  String toString()
-  {
-    return "Book" + bookname;
+  String toString() {
+    return "Book (Name = $bookname, Subject = $subject, Price = $price)";
   }
 }
